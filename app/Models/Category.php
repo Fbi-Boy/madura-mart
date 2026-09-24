db138c6b52fb3b2573034bec491950726cc8e96e
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:28:49 +0700

    feat: add category products relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -22,6 +23,11 @@ class Category extends Model
         return [
             'is_active' => 'boolean',
         ];
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
     }
 
     protected static function booted(): void
