bc220a34f2febfa576d256d991bf32462c594e47
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:35:55 +0700

    feat: add supplier model

---
+++ app/Models/Supplier.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'contact_person',
+        'phone',
+        'email',
+        'address',
+        'city',
+        'is_active',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
+}
