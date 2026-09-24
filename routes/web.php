bc2bfe00e1df5c4efa33208b7ebd5db855736e6f
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 14:37:34 +0700

    Add menu placeholder routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Dashboard\DashboardController;
-use Illuminate\Support\Facades\Route;
+use AppHttpControllersProfileController;
+use AppHttpControllersDashboardDashboardController;
+use IlluminateSupportFacadesRoute;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,6 +12,33 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
+
+    Route::get('/menu/{menu}', function (string $menu) {
+        $menus = [
+            'penjualan' => 'Penjualan',
+            'pembelian' => 'Pembelian',
+            'pesanan' => 'Pesanan',
+            'produk' => 'Produk',
+            'distributor' => 'Distributor',
+            'client' => 'Client',
+            'kurir' => 'Kurir',
+            'laporan-penjualan' => 'Laporan Penjualan',
+            'laporan-pembelian' => 'Laporan Pembelian',
+            'stok' => 'Stok',
+            'transaksi-baru' => 'Transaksi Baru',
+            'riwayat-transaksi' => 'Riwayat Transaksi',
+            'retur' => 'Retur',
+            'buka-shift' => 'Buka Shift',
+            'tutup-shift' => 'Tutup Shift',
+            'riwayat-shift' => 'Riwayat Shift',
+        ];
+
+        abort_unless(isset($menus[$menu]), 404);
+
+        return view('menu.index', [
+            'title' => $menus[$menu],
+        ]);
+    })->name('menu.show');
 
     Route::get('/profile', [ProfileController::class, 'edit'])
         ->name('profile.edit');
