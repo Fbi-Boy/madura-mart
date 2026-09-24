fef74790d21b80e242cd08d5850835161beea67a
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:17:56 +0700

    test: cover /admin/report/stok role access

--- tests/Feature/RoleRouteAccessTest.php
+++ tests/Feature/RoleRouteAccessTest.php
@@ -117,4 +117,13 @@ class RoleRouteAccessTest extends TestCase
             .assertForbidden();
     }
 
+    public function test_customer_cannot_access_admin_report_stok(): void
+    {
+        $user = User::factory()->create(['role' => 'customer']);
+
+        $this->actingAs($user)
+            ->get('/admin/report/stok')
+            .assertForbidden();
+    }
+
 }
