9efe1e617719a4bfc5fff110a48e74d4297c4059
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 23:22:55 +0700

    fix: import sale return item model

--- app/Http/Controllers/Kasir/SaleReturnController.php
+++ app/Http/Controllers/Kasir/SaleReturnController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Sale;
 use App\Models\SaleItem;
 use App\Models\SaleReturn;
+use App\Models\SaleReturnItem;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
