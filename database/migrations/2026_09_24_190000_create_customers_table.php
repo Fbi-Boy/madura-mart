863974da7972cc4a1bd759e0c9257eed735ae7fa
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:41:36 +0700

    fix: correct customer migration namespaces

--- database/migrations/2026_09_24_190000_create_customers_table.php
+++ database/migrations/2026_09_24_190000_create_customers_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\\Database\\Migrations\\Migration;
-use Illuminate\\Database\\Schema\\Blueprint;
-use Illuminate\\Support\\Facades\\Schema;
+use IlluminateDatabaseMigrationsMigration;
+use IlluminateDatabaseSchemaBlueprint;
+use IlluminateSupportFacadesSchema;
 
 return new class extends Migration
 {
