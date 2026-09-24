ffc2e255387022cea57a6ac027a06ce81f75a296
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:24:27 +0700

    fix: correct category factory namespace

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Database\\Factories;
+namespace Database\Factories;
 
-use App\\Models\\Category;
-use Illuminate\\Database\\Eloquent\\Factories\\Factory;
-use Illuminate\\Support\\Str;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class CategoryFactory extends Factory
 {
