477db5cfbd989205ec1e81c59d2cbedcd50c928e
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:17:49 +0700

    test: cover /admin/monitoring/kurir role access

--- tests/Feature/RoleRouteAccessTest.php
+++ tests/Feature/RoleRouteAccessTest.php
@@ -90,4 +90,13 @@ class RoleRouteAccessTest extends TestCase
             .assertForbidden();
     }
 
+    public function test_customer_cannot_access_admin_monitoring_kurir(): void
+    {
+        $user = User::factory()->create(['role' => 'customer']);
+
+        $this->actingAs($user)
+            ->get('/admin/monitoring/kurir')
+            .assertForbidden();
+    }
+
 }
