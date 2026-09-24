1aaa4b3f6931073edac8e36c91017a7e0dd6fbfd
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:24:31 +0700

    fix: correct category seeder namespace

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Database\\Seeders;
+namespace Database\Seeders;
 
-use App\\Models\\Category;
-use Illuminate\\Database\\Seeder;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
