e3a4401d11e8698f940e33a732b4c3d782ba1a99
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 14:49:55 +0700

    Add Transaksi Baru page

---
+++ resources/views/kasir/transaksi-baru/index.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="h-full">
+        <h2 class="text-xl font-semibold text-[#171719] dark:text-white">
+            Transaksi Baru
+        </h2>
+    </div>
+</x-app-layout>
