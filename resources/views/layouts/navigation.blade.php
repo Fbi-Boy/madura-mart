000030371ddf7e0dccd812b37bd34ecf5e99949c
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:51:43 +0700

    feat: add distributor master navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -54,6 +54,7 @@
                 <a href="{{ route('admin.products.index') }}" class="menu-link">Produk Master</a>
                 <a href="{{ route('admin.suppliers.index') }}" class="menu-link">Supplier</a>
                 <a href="{{ route('admin.customers.index') }}" class="menu-link">Customer</a>
+                <a href="{{ route('admin.distributors.index') }}" class="menu-link">Distributor Master</a>
             </div>
 
         @elseif(auth()->user()->role === 'kasir')
