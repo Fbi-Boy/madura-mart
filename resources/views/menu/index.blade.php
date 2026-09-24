eebb090c0c19ef1855f1be17e803253d0b08f985
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 14:37:40 +0700

    Create menu placeholder page

---
+++ resources/views/menu/index.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="h-full flex flex-col">
+        <div class="pt-2 px-1">
+            <h2 class="text-xl sm:text-[22px] font-semibold tracking-[-0.02em] text-[#171719] dark:text-white">
+                {{ $title }}
+            </h2>
+        </div>
+        <div class="flex-1"></div>
+    </div>
+</x-app-layout>
