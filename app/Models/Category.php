3094b41428f70c849d490a2aa4fa8ee0cb1bae4c
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 22:24:24 +0700

    fix: correct category model namespace

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\\Models;
+namespace App\Models;
 
-use Illuminate\\Database\\Eloquent\\Factories\\HasFactory;
-use Illuminate\\Database\\Eloquent\\Model;
-use Illuminate\\Support\\Str;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
