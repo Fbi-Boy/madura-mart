1b5cee06ef661bca5f4811a9e7365619ca01ef85
Fbi-Boy <fabifahruddin06@gmail.com>
2026-09-24 23:01:38 +0700

    feat: register unit master routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CustomerController;
 use App\Http\Controllers\Admin\CourierController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\UnitController;
 use App\Http\Controllers\Admin\DistributorController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SupplierController;
@@ -73,6 +74,7 @@ Route::middleware('auth')->group(function () {
         Route::resource('customers', CustomerController::class)->except(['show']);
         Route::resource('couriers', CourierController::class)->except(['show']);
         Route::resource('users', UserController::class)->except(['show']);
+        Route::resource('units', UnitController::class)->except(['show']);
         Route::resource('distributors', DistributorController::class)->except(['show']);
     });
 
