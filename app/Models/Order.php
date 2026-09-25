f4e4c49a150ca239d600d7bbc98b27db6651c685
Faboy <fabifahruddin06@gmail.com>
2026-09-25 16:18:08 +0700

    feat: connect orders to order items

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -27,6 +28,11 @@ class Order extends Model
             'order_date' => 'datetime',
             'total' => 'decimal:2',
         ];
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
     }
 
     public function customer(): BelongsTo
