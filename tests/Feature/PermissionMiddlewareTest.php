8bfca55119aee6df92d6d2f787f9bef625347cb1
Faboy <fabifahruddin06@gmail.com>
2026-09-26 07:43:22 +0700

    fix: isolate permission middleware tests

--- tests/Feature/PermissionMiddlewareTest.php
+++ tests/Feature/PermissionMiddlewareTest.php
@@ -1,31 +1,29 @@
 <?php
 
-namespace TestsFeature;
+namespace Tests\Feature;
 
 use App\Http\Middleware\PermissionMiddleware;
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Request;
-use Illuminate\Pipeline\Pipeline;
 use Tests\TestCase;
 
 class PermissionMiddlewareTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_allowed_role_can_use_a_permission(): void
     {
         $user = User::factory()->create(['role' => 'purchasing']);
 
-        $response = app(Pipeline::class)
-            ->send(Request::create('/'))
-            ->through([function (Request $request, $next) use ($user) {
-                $request->setUserResolver(fn () => $user);
+        $request = Request::create('/');
+        $request->setUserResolver(fn () => $user);
 
-                return app(PermissionMiddleware::class)->handle(
-                    $request,
-                    $next,
-                    'purchases.manage',
-                );
-            }])
-            ->then(fn () => response('allowed'));
+        $response = app(PermissionMiddleware::class)->handle(
+            $request,
+            fn () => response('allowed'),
+            'purchases.manage',
+        );
 
         $this->assertSame('allowed', $response->getContent());
     }
@@ -34,10 +32,10 @@ class PermissionMiddlewareTest extends TestCase
     {
         $user = User::factory()->create(['role' => 'customer']);
 
-        $this->expectException(\Symfony\Component\HttpKernel\Exception\HttpException::class);
-
         $request = Request::create('/');
         $request->setUserResolver(fn () => $user);
+
+        $this->expectException(\Symfony\Component\HttpKernel\Exception\HttpException::class);
 
         app(PermissionMiddleware::class)->handle(
             $request,
