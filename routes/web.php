28ccd59a7e9221d87f46b12eef24b123c9954ea5
Faboy <fabifahruddin06@gmail.com>
2026-09-26 07:16:09 +0700

    feat(audit): expose admin activity log route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ActivityLogController;
 use App\Http\Controllers\Admin\CustomerController;
 use App\Http\Controllers\Admin\CourierController;
 use App\Http\Controllers\Admin\UserController;
@@ -102,6 +103,7 @@ Route::middleware('auth')->group(function () {
         });
 
     Route::prefix('admin')->name('admin.')->middleware('role:admin,super-admin')->group(function () {
+    Route::get('/activity-logs', [ActivityLogController::class, 'index'])->name('activity-logs.index');
         Route::resource('categories', CategoryController::class)->except(['show']);
         Route::resource('products', ProductController::class)->except(['show']);
         Route::resource('suppliers', SupplierController::class)->except(['show']);
